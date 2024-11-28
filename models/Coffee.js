340310418725a0c2ccac2cad2851532215be5e0c
charles joma <charles.joma@chepnetwork.com>
2024-11-28 18:19:03 +1100

    MODEL :: Remove required fields

--- models/Coffee.js
+++ models/Coffee.js
@@ -1,40 +1,34 @@
 const mongoose = require("mongoose");
 
-const CoffeeSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
+const CoffeeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    number: {
+      type: String,
+      required: true,
+    },
+    coffeeName: {
+      type: String,
+      required: true,
+    },
+    coffeeMilk: {
+      type: String,
+      required: false,
+    },
+    coffeeSize: {
+      type: String,
+      required: true,
+    },
+    extras: {
+      type: Array,
+      required: false,
+    },
   },
-  email: {
-    type: String,
-    required: true
-  },
-  userId: {
-    type: String,
-    required: true
-  },
-  number: {
-    type: String,
-    required: true
-  },
-  coffeeName: {
-    type: String,
-    required: true,
-  },
-  coffeeMilk: {
-    type: String,
-    required: false,
-  },
-  coffeeSize: {
-    type: String,
-    required: true,
-  },
-  extras: {
-    type: Array,
-    required: false
-  }
-  
-}, { collection: 'white_coffees' });
+  { collection: "white_coffees" }
+);
 
 const Coffee = mongoose.model("white_coffees", CoffeeSchema);
 
